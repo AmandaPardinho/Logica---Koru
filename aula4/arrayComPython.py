709412381895a2147eb8004c5eb9d30812fd5958
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-04-06 09:25:36 -0300

    Exercício sobre arrays

---
+++ aula4/arrayComPython.py
@@ -0,0 +1,15 @@
+fruta = "morango"
+print(fruta)
+
+#Iniciar o array frutas informando o nome das frutas selecionadas
+frutas = ["laranja", "maçã", "banana"]
+
+#Exibir o conteúdo do array frutas
+print(frutas)
+
+#Exibir o nome da segunda fruta armazenada no array frutas
+print(frutas[1])
+
+#Quantas frutas temos no array frutas?
+quantidadeFrutas = len(frutas)
+print(quantidadeFrutas)
