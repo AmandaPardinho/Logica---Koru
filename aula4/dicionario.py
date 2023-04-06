9082e1bd197e90bfba8406346549b52720e69c3d
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-04-06 13:03:13 -0300

    Início dos estudos sobre dicionários

---
+++ aula4/dicionario.py
@@ -0,0 +1,11 @@
+#Criar um dicionário de dados para guardar informações do produto
+produto = {
+    "nome": "SmartTV LG",
+    "quantidade": 10,
+    "preco": 3999.90
+}
+
+print(produto)
+
+#Caso queira saber apenas um dos elementos contidos no dicionário "produto"
+print(produto['quantidade'])
