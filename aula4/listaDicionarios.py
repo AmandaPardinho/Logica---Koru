3cc1942c68fe1b1b42899d41d3607489d17c3480
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-04-06 13:55:00 -0300

    Cálculos com listas de dicionários

--- aula4/listaDicionarios.py
+++ aula4/listaDicionarios.py
@@ -16,3 +16,20 @@ while(numerador <= 3):
     numerador = numerador + 1
 
 print(produtos)
+
+# Como saber o preço do 3o. produto?
+print(produtos[2]['preco'])
+
+# Como saber o nome do 1o. produto?
+print(produtos[0]['nome'])
+
+# Somar todos os produtos em estoque
+# Calcular o preço de todos os produtos somados
+totalEstoque = 0
+totalPreco = 0
+for produto in produtos:
+    totalEstoque = totalEstoque + produto['quantidade']
+    totalPreco = totalPreco + produto['preco']
+
+print(f"Total de produtos em estoque: {totalEstoque}")
+print(f"Preço total dos produtos em estoque: {totalPreco}")
