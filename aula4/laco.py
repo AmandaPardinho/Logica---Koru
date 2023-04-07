d867296751b39c3a9e82ea5ae7a2cf521e6ef48a
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-04-07 15:56:38 -0300

    Parte do código do novo exemplo

--- aula4/laco.py
+++ aula4/laco.py
@@ -10,3 +10,9 @@ i = 10
 while(i >= 1):
     print(i)
     i = i - 1
+print("")
+
+numero = int(input("Digite um número de 1 até 10: "))
+while(numero < 1) or (numero > 10):
+    print("Número inválido! Digite um número de 1 até 10")
+    numero = int(input("Digite um número de 1 até 10: "))
