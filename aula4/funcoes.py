969ecdc9101116e9ceca079f7dab28ebf84d1c14
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-04-08 13:11:07 -0300

    Criando uma função

--- aula4/funcoes.py
+++ aula4/funcoes.py
@@ -1,3 +1,12 @@
 # Calcular 4 ao cubo
 print(4*4*4)
 print(pow(4, 3))
+
+# Declarar a função soma(), que recebe dois números inteiros e retorna a soma entre eles
+    # def => definition => usada para declarar uma função
+def soma(num1, num2):
+    total = num1 + num2
+    return total
+
+soma1 = soma(5, 4)
+print(soma1)
