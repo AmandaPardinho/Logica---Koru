908f98ea342f5584d72d3c7458ef5bfdae20e69a
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-04-08 14:06:08 -0300

    Usando dicionários para mostrar o conteúdo de um arquivo csv

--- aula4/exemploCsv.py
+++ aula4/exemploCsv.py
@@ -1,8 +1,19 @@
 import csv
 with open("produtos_exemplo.csv", newline = '\n') as csvfile:
-    leitor = csv.reader(csvfile, delimiter = ',')
-    next(leitor)
+    leitor = csv.reader(csvfile, delimiter = ',') # delimiter => mostra como os atributos são separados no arquivo
+    next(leitor) # next => retira o cabeçalho
 
+    #for linha in leitor:
+     #   print(linha)
+      # print(linha[0])
+
+ # Usando dicionário para ler o arquivo
+    produtos = []
     for linha in leitor:
-        print(linha)
-        print(linha[0])
+        produto = {
+            "codigo": int(linha[0]),
+            "nome": linha[1],
+            "preco": float(linha[2])
+        }
+        produtos.append(produto)
+print(produtos)
